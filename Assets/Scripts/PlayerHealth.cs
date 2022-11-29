14b0c7af2c60d4073588310f43ed60fb9d40e7a9
SOUnit <workbookwork7@gmail.com>
2022-11-28 23:51:59 -0800

    add game over menu

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
+    [SerializeField]
+    private GameOverHandler gameOverHandler;
+
     public void Crash()
     {
+        gameOverHandler.EndGame();
+
         gameObject.SetActive(false);
     }
 }
